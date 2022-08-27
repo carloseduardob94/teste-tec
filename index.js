943ec32fc9974dbc5d7ba9d70d3a4e9172ac77a6
Eduardo <educonts08@gmail.com>
2022-08-27 16:38:37 -0300

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { ciclos } from './Students.js'
 
-export class Aluno {
+class Aluno {
   constructor(name, workTogheter = []) {
     this.name = name
     this.workTogheter = workTogheter.filter(student => student !== name)
@@ -42,35 +42,6 @@ const studeents = alunosCadastrados
 
 let newTeam = []
 let ciclo3 = []
-
-// function createNewTeam() {
-//   let hits = 0
-//   while (newTeam.length < 10) {
-//     const raffle = Math.floor(Math.random() * alunosCadastrados.length)
-//     const raffle2 = Math.floor(Math.random() * alunosCadastrados.length)
-
-//     const aluno1 = alunosCadastrados[raffle]
-//     const aluno2 = alunosCadastrados[raffle2]
-
-//     if (hits == 20) {
-//       newTeam = []
-//       alunosCadastrados = studeents
-//       hits = 0
-//     }
-//     if (raffle == raffle2) {
-//       hits++
-//     } else {
-//       const workTogheter = aluno1.checkWorkTogheter(aluno2.name)
-//       if (!workTogheter) {
-//         alunosCadastrados = alunosCadastrados.filter(
-//           aluno => aluno.name !== aluno1.name && aluno.name !== aluno2.name
-//         )
-//         const dupla = [aluno1.name, aluno2.name]
-//         newTeam.push(dupla)
-//       }
-//     }
-//   }
-// }
 
 class createHTML {
   constructor(root) {
